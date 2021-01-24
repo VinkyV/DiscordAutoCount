b7863be53614c3a3e3abff7ca3f392e492f807a5
VinkyV <75137786+VinkyV@users.noreply.github.com>
2021-01-24 22:04:07 +0300

    Add files via upload

---
+++ AutoCounter.py
@@ -0,0 +1,20 @@
+import pyautogui
+from time import sleep
+
+print("Кто прочитал тот приёмный! by NightMaidBot#7660")
+print("Дабавьте 2 к скорость от медленного режима/Add 2 of the slow mode")
+swr = input("следуещее число/Next number >")
+spd = int(input("Скорость/Speed >"))
+print("Открой дискорд,нажми на поле ввода и жди(комп не трогай)/Open Discord, touch the input and wait(don't use PC)")
+sleep(10)
+def turbo(count, speed):
+    while count!= 10001:
+        print(count)
+        count=str(count)
+        pyautogui.write(count)
+        pyautogui.press('enter')
+        sleep(speed)
+        count=int(count)
+        count=count+1;
+    turbo(swr, spd)
+turbo(swr, spd)
